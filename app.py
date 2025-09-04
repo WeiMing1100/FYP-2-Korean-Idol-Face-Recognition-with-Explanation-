d5dc9b70dac004ef6f61b467952362d013d41640
jayden1100 <chuwei1100@gmail.com>
2025-09-05 03:59:11 +0800

    added st.spinner

--- app.py
+++ app.py
@@ -213,108 +213,109 @@ if uploaded_image is None:
     </div>
     """, unsafe_allow_html=True)
 elif uploaded_image is not None:
-    file_bytes = np.asarray(bytearray(uploaded_image.read()), dtype=np.uint8)
-    img_bgr = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+    with st.spinner("Loading... Please wait."):
+        file_bytes = np.asarray(bytearray(uploaded_image.read()), dtype=np.uint8)
+        img_bgr = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 
 
 
-    grad_cam = get_grad_cam()
-    extracted_face= extract_faces(img_rgb)
-    if extracted_face is None:
-        st.text("No face found in the image")
-    elif extracted_face is not None:
-        # I have already called preprocess_image in apply_grad_cam (which is called inside visualize_gradCAM_results)
-        is_kpop_idol, cam, cam_heatmap, overlaid_image = visualize_gradCAM_results(img_rgb, extracted_face, model, grad_cam)
+        grad_cam = get_grad_cam()
+        extracted_face= extract_faces(img_rgb)
+        if extracted_face is None:
+            st.text("No face found in the image")
+        elif extracted_face is not None:
+            # I have already called preprocess_image in apply_grad_cam (which is called inside visualize_gradCAM_results)
+            is_kpop_idol, cam, cam_heatmap, overlaid_image = visualize_gradCAM_results(img_rgb, extracted_face, model, grad_cam)
 
-        if is_kpop_idol:
-            st.divider()
-            # text_explanation = generate_textual_explanation(cam, retinaface_landmarks, overlaid_image)
-            top_score, sorted_regions, regions_text = generate_textual_explanation_using_mediapipe_landmarks(cam, extracted_face, overlaid_image)
+            if is_kpop_idol:
+                st.divider()
+                # text_explanation = generate_textual_explanation(cam, retinaface_landmarks, overlaid_image)
+                top_score, sorted_regions, regions_text = generate_textual_explanation_using_mediapipe_landmarks(cam, extracted_face, overlaid_image)
 
-            # dataframe for sorted_regions
-            df_scores = pd.DataFrame(sorted_regions, columns=["Region", "Score"])
-            styled_df = df_scores.style.background_gradient(cmap="Blues")
-            st.dataframe(styled_df, width="stretch", height="auto", hide_index=True)
-            st.caption("*Note: Score indicates how much the model focuses for each region*")
+                # dataframe for sorted_regions
+                df_scores = pd.DataFrame(sorted_regions, columns=["Region", "Score"])
+                styled_df = df_scores.style.background_gradient(cmap="Blues")
+                st.dataframe(styled_df, width="stretch", height="auto", hide_index=True)
+                st.caption("*Note: Score indicates how much the model focuses for each region*")
 
-            if top_score < 0.2: # weak focus
+                if top_score < 0.2: # weak focus
+                    st.markdown(f"""
+                    <div class="custom-markdown-class">
+                        The model distributed attention across the whole face.
+                    </div>
+                    """, unsafe_allow_html=True)
+                else:
+                    st.markdown(f"""
+                    <div class="custom-markdown-class">
+                        The model focused mostly on the 
+                        <span style="color: orange;">{regions_text}</span>
+                        when identifying this idol!
+                    </div>
+                    """, unsafe_allow_html=True)
+
+            elif not is_kpop_idol:
+                idol_embeddings = np.load("idol_embeddings.npy")
+                idol_labels = np.load("idol_labels.npy", allow_pickle=True)  # if labels are strings
+                query_embeddings = get_embedding(extracted_face)
+
+                # visualize embeddings
+                best_idx, similarity_score = visualize_embeddings(idol_embeddings, query_embeddings, idol_labels)
+
+                st.caption(
+                    "Note: The 2D t-SNE plot is only a simplified view of the 512-dimensional embedding space."
+                    "Points that look far apart here may still be very close in the original high-dimensional space,"
+                    "so global distances(distance between each cluster) in this plot may not always reflect the true similarity."
+                )
+
+                st.divider()
+
+                predicted_label = idol_labels[best_idx]
+                similarity_score_percent = str(np.round(similarity_score, decimals=4)*100)[:5]
+                # print("Predicted idol:", predicted_label)
+                # print("Similarity:", similarity_score)
+
+                idol_image_pair = {
+                    "an yujin": os.path.join("pairs", "an yujin", "yujin.png"),
+                    "chaeryeong": os.path.join("pairs", "chaeryeong", "chaeryeong.png"),
+                    "hani": os.path.join("pairs", "hani", "hani.png"),
+                    "heejin": os.path.join("pairs", "heejin", "heejin.png"),
+                    "irene": os.path.join("pairs", "irene", "irene.png"),
+                    "kwon eunbi": os.path.join("pairs", "kwon eunbi", "eunbi.png"),
+                    "lee chaeyeon": os.path.join("pairs", "lee chaeyeon", "chaeyeon.png"),
+                    "lisa": os.path.join("pairs", "lisa", "lisa.png"),
+                    "mina": os.path.join("pairs", "mina", "mina.png"),
+                    "momo": os.path.join("pairs", "momo", "momo.png"),
+                    "moonbyul": os.path.join("pairs", "moonbyul", "moonbyul.png"),
+                    "nayeon": os.path.join("pairs", "nayeon", "nayeon.png"),
+                    "rose": os.path.join("pairs", "rose", "rose.png"),
+                    "ryujin": os.path.join("pairs", "ryujin", "ryujin.png"),
+                    "seulgi": os.path.join("pairs", "seulgi", "seulgi.png"),
+                    "sinB": os.path.join("pairs", "sinB", "sinb.png"),
+                    "soojin": os.path.join("pairs", "soojin", "soojin.png"),
+                    "soyeon": os.path.join("pairs", "soyeon", "soyeon.png"),
+                    "tzuyu": os.path.join("pairs", "tzuyu", "tzuyu.png"),
+                    "wheein": os.path.join("pairs", "wheein", "wheein.png"),
+                    "yeji": os.path.join("pairs", "yeji", "yeji.png"),
+                    "yena": os.path.join("pairs", "yena", "yena.png"),
+                    "yuna": os.path.join("pairs", "yuna", "yuna.png"),
+                    "yuqi": os.path.join("pairs", "yuqi", "yuqi.png"),
+                    "yves": os.path.join("pairs", "yves", "yves.png"),
+                }
+
+                similar_image = idol_image_pair[predicted_label]
+                capitalized_predicted_label = predicted_label.capitalize()
+                visualize_similar_images(uploaded_image, similar_image, capitalized_predicted_label)
+
                 st.markdown(f"""
                 <div class="custom-markdown-class">
-                    The model distributed attention across the whole face.
+                    Your submitted image is probably not in the list of Kpop Idols. 
+                    This person looks the most similar to 
+                    <span style="color: orange;">{capitalized_predicted_label}</span>, 
+                    with similarity score of 
+                    <span style="color: orange;">{similarity_score_percent}%</span>!
                 </div>
                 """, unsafe_allow_html=True)
-            else:
-                st.markdown(f"""
-                <div class="custom-markdown-class">
-                    The model focused mostly on the 
-                    <span style="color: orange;">{regions_text}</span>
-                    when identifying this idol!
-                </div>
-                """, unsafe_allow_html=True)
-
-        elif not is_kpop_idol:
-            idol_embeddings = np.load("idol_embeddings.npy")
-            idol_labels = np.load("idol_labels.npy", allow_pickle=True)  # if labels are strings
-            query_embeddings = get_embedding(extracted_face)
-
-            # visualize embeddings
-            best_idx, similarity_score = visualize_embeddings(idol_embeddings, query_embeddings, idol_labels)
-
-            st.caption(
-                "Note: The 2D t-SNE plot is only a simplified view of the 512-dimensional embedding space."
-                "Points that look far apart here may still be very close in the original high-dimensional space,"
-                "so global distances(distance between each cluster) in this plot may not always reflect the true similarity."
-            )
-
-            st.divider()
-
-            predicted_label = idol_labels[best_idx]
-            similarity_score_percent = str(np.round(similarity_score, decimals=4)*100)[:5]
-            # print("Predicted idol:", predicted_label)
-            # print("Similarity:", similarity_score)
-
-            idol_image_pair = {
-                "an yujin": os.path.join("pairs", "an yujin", "yujin.png"),
-                "chaeryeong": os.path.join("pairs", "chaeryeong", "chaeryeong.png"),
-                "hani": os.path.join("pairs", "hani", "hani.png"),
-                "heejin": os.path.join("pairs", "heejin", "heejin.png"),
-                "irene": os.path.join("pairs", "irene", "irene.png"),
-                "kwon eunbi": os.path.join("pairs", "kwon eunbi", "eunbi.png"),
-                "lee chaeyeon": os.path.join("pairs", "lee chaeyeon", "chaeyeon.png"),
-                "lisa": os.path.join("pairs", "lisa", "lisa.png"),
-                "mina": os.path.join("pairs", "mina", "mina.png"),
-                "momo": os.path.join("pairs", "momo", "momo.png"),
-                "moonbyul": os.path.join("pairs", "moonbyul", "moonbyul.png"),
-                "nayeon": os.path.join("pairs", "nayeon", "nayeon.png"),
-                "rose": os.path.join("pairs", "rose", "rose.png"),
-                "ryujin": os.path.join("pairs", "ryujin", "ryujin.png"),
-                "seulgi": os.path.join("pairs", "seulgi", "seulgi.png"),
-                "sinB": os.path.join("pairs", "sinB", "sinb.png"),
-                "soojin": os.path.join("pairs", "soojin", "soojin.png"),
-                "soyeon": os.path.join("pairs", "soyeon", "soyeon.png"),
-                "tzuyu": os.path.join("pairs", "tzuyu", "tzuyu.png"),
-                "wheein": os.path.join("pairs", "wheein", "wheein.png"),
-                "yeji": os.path.join("pairs", "yeji", "yeji.png"),
-                "yena": os.path.join("pairs", "yena", "yena.png"),
-                "yuna": os.path.join("pairs", "yuna", "yuna.png"),
-                "yuqi": os.path.join("pairs", "yuqi", "yuqi.png"),
-                "yves": os.path.join("pairs", "yves", "yves.png"),
-            }
-
-            similar_image = idol_image_pair[predicted_label]
-            capitalized_predicted_label = predicted_label.capitalize()
-            visualize_similar_images(uploaded_image, similar_image, capitalized_predicted_label)
-
-            st.markdown(f"""
-            <div class="custom-markdown-class">
-                Your submitted image is probably not in the list of Kpop Idols. 
-                This person looks the most similar to 
-                <span style="color: orange;">{capitalized_predicted_label}</span>, 
-                with similarity score of 
-                <span style="color: orange;">{similarity_score_percent}%</span>!
-            </div>
-            """, unsafe_allow_html=True)
 
 
 
